460451f90cfab59088a6610455cda81a045c592d
kjunice4 <37306150+kjunice4@users.noreply.github.com>
2022-05-13 11:05:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ Builder.load_string("""
                 root.manager.transition.direction = "left" 
                 
         Button:
-            font_size: 50
+            font_size: '20sp'
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 200
@@ -31,11 +31,21 @@ Builder.load_string("""
                 root.manager.transition.direction = "left"         
                 
         Button:
-            font_size: 50
+            font_size: '20sp'
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
-            height: 200
-            text: "KSquared-Mathematics : Pythagorean Calculator"
+            height: 100
+            text: "KSquared-Mathematics :"
+            on_release:
+                app.root.current = "Menu"
+                root.manager.transition.direction = "left" 
+                
+        Button:
+            font_size: '20sp'
+            background_color: 0, 0 , 0 , 1
+            size_hint_y: None
+            height: 100
+            text: "Pythagorean Calculator"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
@@ -63,7 +73,7 @@ Builder.load_string("""
             height: self.minimum_height
             
             Label:
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -71,7 +81,7 @@ Builder.load_string("""
             
             Button:
                 text: "Pythagorean Calculator"   
-                font_size: 75
+                font_size: '20sp'
                 background_color: 0, 0 , 1 , 1
                 size_hint_y: None
                 height: 200
@@ -81,7 +91,7 @@ Builder.load_string("""
                     root.manager.transition.direction = "left" 
                     
             Button:
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -91,7 +101,7 @@ Builder.load_string("""
                     webbrowser.open('https://www.ksquaredmathematics.com/subscribe') 
                     
             Button:
-                font_size: 75
+                font_size: '20sp'
                 background_color: 1, 0, 1, 1
                 size_hint_y: None
                 height: 200
@@ -102,7 +112,7 @@ Builder.load_string("""
                     root.manager.transition.direction = "left"
                     
             Label:
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -111,8 +121,8 @@ Builder.load_string("""
             Image:
                 source: 'KSquared_QR.png'
                 size_hint_y: None
-                height: 1000
-                width: 1000
+                height: 800
+                width: 800
 """)
 
 #Updates
@@ -135,7 +145,7 @@ Builder.load_string("""
             height: self.minimum_height
             
             Label:
-                font_size: 60
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -144,7 +154,7 @@ Builder.load_string("""
             Button:
                 id: steps
                 text: "Menu"   
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 background_color: 0, 0 , 1 , 1
                 height: 200
@@ -154,14 +164,14 @@ Builder.load_string("""
                     root.manager.transition.direction = "right" 
                     
             Label:
-                font_size: 40
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
                 text: "Pythagorean Calculator v0.1"
                 
             Label:
-                font_size: 40
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -189,7 +199,7 @@ Builder.load_string("""
             height: self.minimum_height
             
             Label:
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -206,7 +216,7 @@ Builder.load_string("""
                 
                 Button:
                     text: "Menu"   
-                    font_size: 75
+                    font_size: '20sp'
                     size_hint_y: None
                     height: 200
                     padding: 10, 10
@@ -218,7 +228,7 @@ Builder.load_string("""
                 Button:
                     id: steps
                     text: "Clear All"   
-                    font_size: 75
+                    font_size: '20sp'
                     size_hint_y: None
                     background_color: 1, 0 , 0 , 1
                     height: 200
@@ -230,7 +240,7 @@ Builder.load_string("""
                         list_of_steps.clear_widgets()            
                     
             Label:
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
@@ -241,7 +251,7 @@ Builder.load_string("""
                 text: a.text
                 multiline: False
                 hint_text: "a ="
-                font_size: 125
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10
@@ -253,7 +263,7 @@ Builder.load_string("""
                 text: b.text
                 multiline: False
                 hint_text:"b ="
-                font_size: 125
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10          
@@ -264,7 +274,7 @@ Builder.load_string("""
                 text: c.text
                 multiline: False
                 hint_text:"c ="
-                font_size: 125
+                font_size: '20sp'
                 size_hint_y: None
                 height: 200
                 padding: 10          
@@ -273,7 +283,7 @@ Builder.load_string("""
             Button:
                 id: steps
                 text: "Calculate"   
-                font_size: 75
+                font_size: '20sp'
                 size_hint_y: None
                 background_color: 0, 1 , 0 , 1
                 height: 200
@@ -339,19 +349,19 @@ class Pythagorean(Screen):
                 
                 if str(entry_evaled) == str(c_squared):
                     print("entry_evaled = c")
-                    self.ids.list_of_steps.add_widget(Label(text= str(a) + "\u00B2"  + " + " + str(b) + "\u00B2" + " = " + str(c) + "\u00B2", font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= str(a) + "\u00B2"  + " + " + str(b) + "\u00B2" + " = " + str(c) + "\u00B2", font_size = '20sp', size_hint_y= None, height=100))
                     
                     a_squared = str(int(a)**2)
                     b_squared = str(int(b)**2)
                     
                     ab = str(int(a_squared) + int(b_squared))
                     
-                    self.ids.list_of_steps.add_widget(Label(text= str(a_squared) + " + " + str(b_squared) + " = " + str(c_squared), font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= str(ab) + " = " + str(c_squared), font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= str(a_squared) + " + " + str(b_squared) + " = " + str(c_squared), font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= str(ab) + " = " + str(c_squared), font_size = '20sp', size_hint_y= None, height=100))
 
                 else:
                     print("entry_evaled does not = c!!!!!!!")
-                    self.ids.list_of_steps.add_widget(Label(text= "Input does not equate to triangle!", font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "Input does not equate to triangle!", font_size = '20sp', size_hint_y= None, height=100))
                     self.layouts.append(layout)
             
             # 3 + b = 5 OR a + 4 = 5
@@ -366,96 +376,96 @@ class Pythagorean(Screen):
                     print()
                     print("Solve for a!")
                     
-                    self.ids.list_of_steps.add_widget(Label(text= str(a) + "\u00B2"  + " + " + str(b) + "\u00B2" + " = c\u00B2", font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= str(a_squared)  + " + " + str(b_squared) + " = c\u00B2", font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= str(a) + "\u00B2"  + " + " + str(b) + "\u00B2" + " = c\u00B2", font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= str(a_squared)  + " + " + str(b_squared) + " = c\u00B2", font_size = '20sp', size_hint_y= None, height=100))
 
                     a_plus_b = int(a_squared) + int(b_squared)
                     print("a_plus_b = ",a_plus_b)
                     
-                    self.ids.list_of_steps.add_widget(Label(text= "c\u00B2" + " = " + str(a_plus_b) , font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "\u221a(" + " c\u00B2)" + " = " + "\u221a(" + str(a_plus_b) + ")" , font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "c\u00B2" + " = " + str(a_plus_b) , font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "\u221a(" + " c\u00B2)" + " = " + "\u221a(" + str(a_plus_b) + ")" , font_size = '20sp', size_hint_y= None, height=100))
 
                     a_plus_b_root = str(float(a_plus_b)**.5)
                     print("a_plus_b_root = ",a_plus_b_root)
 
                     if a_plus_b_root[-2] == "." and a_plus_b_root[-1] == "0":
-                        self.ids.list_of_steps.add_widget(Label(text= "c = " + format(float(a_plus_b_root),","), font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "c = " + format(float(a_plus_b_root),","), font_size = '20sp', size_hint_y= None, height=100))
                         self.layouts.append(layout)
                     else:
-                        self.ids.list_of_steps.add_widget(Label(text= "c = " + "\u221a(" + format(float(a_plus_b),",") + ")", font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "c = " + "\u221a(" + format(float(a_plus_b),",") + ")", font_size = '20sp', size_hint_y= None, height=100))
                         self.layouts.append(layout)
                                         
                     if a_plus_b_root == "0.0":
-                        self.ids.list_of_steps.add_widget(Label(text= "0.0 cannot form a valid line for triangle" ,font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "0.0 cannot form a valid line for triangle" ,font_size = '20sp', size_hint_y= None, height=100))
                     
                 elif int(entry[0]) > 0 and int(entry[2]) > 0:
                     print("a and c are valid")
                     print()
                     print("Solve for b!")
                     
-                    self.ids.list_of_steps.add_widget(Label(text= str(a) + "\u00B2"  + " + " + "b\u00B2" + " = " + str(c) + "\u00B2", font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "b\u00B2" + " = " + str(c) + "\u00B2" + " - " + str(a) + "\u00B2" , font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "b\u00B2" + " = " + str(c_squared) + " - " + str(a_squared) , font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= str(a) + "\u00B2"  + " + " + "b\u00B2" + " = " + str(c) + "\u00B2", font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "b\u00B2" + " = " + str(c) + "\u00B2" + " - " + str(a) + "\u00B2" , font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "b\u00B2" + " = " + str(c_squared) + " - " + str(a_squared) , font_size = '20sp', size_hint_y= None, height=100))
                     
                     c_minus_a = int(c_squared) - int(a_squared)
                     print("c_minus_a = ",c_minus_a)
                     
-                    self.ids.list_of_steps.add_widget(Label(text= "b\u00B2" + " = " + str(c_minus_a) , font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "\u221a(" + " b\u00B2)" + " = " + "\u221a(" + str(c_minus_a) + ")" , font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "b\u00B2" + " = " + str(c_minus_a) , font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "\u221a(" + " b\u00B2)" + " = " + "\u221a(" + str(c_minus_a) + ")" , font_size = '20sp', size_hint_y= None, height=100))
                     
                     c_minus_a_root = str(float(c_minus_a)**.5)
                     print("c_minus_a_root = ",c_minus_a_root)
 
                     if c_minus_a_root[-2] == "." and c_minus_a_root[-1] == "0":
-                        self.ids.list_of_steps.add_widget(Label(text= "b = " + format(float(c_minus_a_root),","), font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "b = " + format(float(c_minus_a_root),","), font_size = '20sp', size_hint_y= None, height=100))
                         self.layouts.append(layout)
                     else:
-                        self.ids.list_of_steps.add_widget(Label(text= "b = " + "\u221a(" + format(float(c_minus_a),",") + ")", font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "b = " + "\u221a(" + format(float(c_minus_a),",") + ")", font_size = '20sp', size_hint_y= None, height=100))
                         self.layouts.append(layout)
                     
                     if c_minus_a_root == "0.0":
-                        self.ids.list_of_steps.add_widget(Label(text= "0.0 cannot form a valid line for triangle" ,font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "0.0 cannot form a valid line for triangle" ,font_size = '20sp', size_hint_y= None, height=100))
                     
                 elif int(entry[1]) > 0 and int(entry[2]) > 0:
                     print("b and c are valid")
                     print()
                     print("Solve for a!")
                     
-                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2"  + " + " + str(b) + "\u00B2" + " = " + str(c) + "\u00B2", font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2" + " = " + str(c) + "\u00B2" + " - " + str(b) + "\u00B2" , font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2" + " = " + str(c_squared) + " - " + str(b_squared) , font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2"  + " + " + str(b) + "\u00B2" + " = " + str(c) + "\u00B2", font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2" + " = " + str(c) + "\u00B2" + " - " + str(b) + "\u00B2" , font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2" + " = " + str(c_squared) + " - " + str(b_squared) , font_size = '20sp', size_hint_y= None, height=100))
                     
                     c_minus_b = int(c_squared) - int(b_squared)
                     print("c_minus_b = ",c_minus_b)
                     
-                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2" + " = " + str(c_minus_b) , font_size = 50, size_hint_y= None, height=100))
-                    self.ids.list_of_steps.add_widget(Label(text= "\u221a(" + " a\u00B2)" + " = " + "\u221a(" + str(c_minus_b) + ")" , font_size = 50, size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "a\u00B2" + " = " + str(c_minus_b) , font_size = '20sp', size_hint_y= None, height=100))
+                    self.ids.list_of_steps.add_widget(Label(text= "\u221a(" + " a\u00B2)" + " = " + "\u221a(" + str(c_minus_b) + ")" , font_size = '20sp', size_hint_y= None, height=100))
                     
                     c_minus_b_root = str(float(c_minus_b)**.5)
                     print("c_minus_b_root = ",c_minus_b_root)
 
                     if c_minus_b_root[-2] == "." and c_minus_b_root[-1] == "0":
-                        self.ids.list_of_steps.add_widget(Label(text= "a = " + format(float(c_minus_b_root),","), font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "a = " + format(float(c_minus_b_root),","), font_size = '20sp', size_hint_y= None, height=100))
                         self.layouts.append(layout)
                     else:
-                        self.ids.list_of_steps.add_widget(Label(text= "a = " + "\u221a(" + format(float(c_minus_b),",") + ")", font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "a = " + "\u221a(" + format(float(c_minus_b),",") + ")", font_size = '20sp', size_hint_y= None, height=100))
                         self.layouts.append(layout)
                         
                     if c_minus_b_root == "0.0":
-                        self.ids.list_of_steps.add_widget(Label(text= "0.0 cannot form a valid line for triangle" ,font_size = 50, size_hint_y= None, height=100))
+                        self.ids.list_of_steps.add_widget(Label(text= "0.0 cannot form a valid line for triangle" ,font_size = '20sp', size_hint_y= None, height=100))
                     
                 
             else:
                 print("Invalid length")                
             
         except Exception:
-            self.ids.list_of_steps.add_widget(Label(text= "Invalid Input" ,font_size = 50, size_hint_y= None, height=100))
+            self.ids.list_of_steps.add_widget(Label(text= "Invalid Input" ,font_size = '20sp', size_hint_y= None, height=100))
             self.layouts.append(layout)
             
         
         if entry[0].count("-") > 0 or entry[1].count("-") > 0 or entry[2].count("-") > 0:
             print("entry neg: ",entry)
-            self.ids.list_of_steps.add_widget(Label(text= "Cannot have negative sides of a triangle!" ,font_size = 50, size_hint_y= None, height=100))
+            self.ids.list_of_steps.add_widget(Label(text= "Cannot have negative sides of a triangle!" ,font_size = '20sp', size_hint_y= None, height=100))
             self.layouts.append(layout)
         
 class Homepage(Screen):
